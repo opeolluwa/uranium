081345ba5a0dae7398e8114ee224e639fcfabf5a
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-16 09:21:16 +0100

    FEAT -> configured PWA

---
+++ views/assets/NotificationView.335a290f.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as t}from"./index.5b0a6421.js";const c={name:"NotificationView",components:{}};function n(a,i,s,r,p,_){return e(),t("div")}const m=o(c,[["render",n]]);export{m as default};
