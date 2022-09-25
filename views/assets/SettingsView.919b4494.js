726420ed7f9d6a2f4156eea69593fd544d8fc931
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-25 08:33:54 +0100

    FIXES -> fixed error rendering static files

--- views/assets/SettingsView.919b4494.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,a as n}from"./index.f66dd100.js";const s={name:"SettingsView",components:{}};function o(c,a,r,i,p,_){return t(),n("h1",null,"settings")}const l=e(s,[["render",o]]);export{l as default};
