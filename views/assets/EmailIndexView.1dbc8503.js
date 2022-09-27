5a453e0529f8985b01a8514ad1b90dc32dece7cc
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-27 23:01:47 +0100

    UPDATE

--- views/assets/EmailIndexView.1dbc8503.js
+++
@@ -1,1 +0,0 @@
-import{d as o,_ as n,r as t,o as r,c as s}from"./index.81bfaa80.js";const a=o({name:"IndexEmailView"});function c(i,p,_,m,d,f){const e=t("RouterView");return r(),s(e)}const u=n(a,[["render",c]]);export{u as default};
