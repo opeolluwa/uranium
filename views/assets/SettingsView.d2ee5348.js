128d42f6892b69390f2dec01eda6e556eeb5e967
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-28 23:00:25 +0100

    UPADTE

--- views/assets/SettingsView.d2ee5348.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,a as n}from"./index.8841255d.js";const s={name:"SettingsView",components:{}};function o(c,a,r,i,p,_){return t(),n("h1",null,"settings")}const l=e(s,[["render",o]]);export{l as default};
