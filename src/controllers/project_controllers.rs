de356addba102b176173a0ff0514f8ac6005688a
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-29 21:56:07 +0100

    CHORE -> mounted project routes

--- src/controllers/project_controllers.rs
+++ src/controllers/project_controllers.rs
@@ -1,27 +1,20 @@
+use axum::response::IntoResponse;
+
 ///create new project
-/// accept the following data 
-/// - projectName  a unique name for the project 
-/// - projectDescription - the project desription 
-/// - repoUrl - the project repository 
-/// 
-pub fn add_project(){
+/// accept the following data
+/// - projectName  a unique name for the project
+/// - projectDescription - the project desription
+/// - repoUrl - the project repository
+///
+pub async fn add_project() -> impl IntoResponse {}
 
-}
-
-
-///edit project 
-/// accept the project id 
-/// find the project 
-/// effect edits 
+///edit project
+/// accept the project id
+/// find the project
+/// effect edits
 /// return updated project object
-pub fn edit_project(){
+pub async fn edit_project() -> impl IntoResponse {}
 
-}
-
-
-
-///get all projects 
+///get all projects
 /// retrieve all project with pagination
-pub fn get_all_projects(){
-    
-}
+pub async fn get_all_projects() -> impl IntoResponse {}
--- src/routes/root.rs
+++ src/routes/root.rs
@@ -1,8 +1,9 @@
-use super::{auth_routes, email_routes};
+use super::{auth_routes, email_routes, project_routes};
 use axum::Router;
 
 pub fn router() -> axum::Router {
     Router::new()
         .nest("/auth", auth_routes::routes())
         .nest("/emails", email_routes::routes())
+        .nest("/projects", project_routes::routes())
 }
