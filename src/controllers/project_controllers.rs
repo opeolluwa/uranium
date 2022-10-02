988906e172a978073a46349c3d08438c8fe019ed
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-10-02 21:46:05 +0100

    UPDATE

--- src/controllers/project_controllers.rs
+++ src/controllers/project_controllers.rs
@@ -1,12 +1,83 @@
-use axum::response::IntoResponse;
+use crate::{
+    models::projects::{ProjectInformation, ProjectsModel},
+    shared::api_response::{ApiErrorResponse, ApiSuccessResponse, EnumerateFields},
+};
+use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
+use sqlx::PgPool;
+use uuid::Uuid;
 
 ///create new project
 /// accept the following data
 /// - projectName  a unique name for the project
-/// - projectDescription - the project desription
+/// - projectDescription - the project description
 /// - repoUrl - the project repository
 ///
-pub async fn add_project() -> impl IntoResponse {}
+pub async fn add_project(
+    Json(payload): Json<ProjectInformation>,
+    Extension(database): Extension<PgPool>,
+) -> Result<(StatusCode, Json<ApiSuccessResponse<ProjectsModel>>), ApiErrorResponse> {
+    //check through the fields to see that no field was badly formatted
+    let entries = &payload.collect_as_strings();
+    let mut bad_request_errors: Vec<String> = Vec::new();
+    for (key, value) in entries {
+        if value.is_empty() {
+            let error = format!("{key} is empty");
+            bad_request_errors.push(error);
+        }
+    }
+
+    // destructure the payload
+    let ProjectInformation {
+        // the project name
+        name: project_name,
+        //the project description
+        description: project_description,
+        //the technologies used, stored as array of string
+        technologies_used,
+        //the date the project was added to the database
+        date_added,
+        //the project repository url
+        repo_url,
+        //the url of the deployed application if any
+        live_url,
+    } = &payload;
+
+    // save the new project
+    /*
+     * generate a UUID and hash the user password,
+     * go on to save the hashed password along side other details
+     * cat any error along the way
+     */
+    let project_id = Uuid::new_v4();
+    let new_project =  sqlx::query_as::<_, ProjectsModel>(
+        "INSERT INTO project_information (id, name, description, date_added, technologies_used, repo_url, live_url) VALUES ($1, $2, $3, $4, $5, $, $7) ON CONFLICT (name) DO NOTHING RETURNING *",
+    )
+    .bind(Some(project_id))
+    .bind(Some(project_name))
+    .bind(Some(date_added))
+    .bind(Some(project_description))
+    .bind(Some(technologies_used))
+    .bind(Some(repo_url))
+    .bind(Some(live_url))
+    .fetch_one(&database).await;
+
+    //handle error
+    match new_project {
+        Ok(project) => {
+            //build the response body
+            let response_body: ApiSuccessResponse<ProjectsModel> = ApiSuccessResponse {
+                success: true,
+                message: "project successfully added ".to_string(),
+                data: Some(project),
+            };
+            //send the response
+            Ok((StatusCode::CREATED, Json(response_body)))
+        }
+        Err(error_message) => Err(ApiErrorResponse::ServerError {
+            error: error_message.to_string(),
+        }),
+    }
+}
 
 ///edit project
 /// accept the project id
@@ -17,4 +88,13 @@ pub async fn edit_project() -> impl IntoResponse {}
 
 ///get all projects
 /// retrieve all project with pagination
-pub async fn get_all_projects() -> impl IntoResponse {}
+pub async fn get_all_projects(
+    Extension(database): Extension<PgPool>,
+) -> impl IntoResponse {
+    //fetch all projects ...
+    //TODO: implement pagination logic
+     let fetched_projects = sqlx::query_as::<_, ProjectsModel>("SELECT * FROM project_information")
+        .fetch(&database);
+ 
+    todo!()
+}
--- src/models/projects.rs
+++ src/models/projects.rs
@@ -1,15 +1,13 @@
-use std::collections::HashMap;
-
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
 use crate::shared::api_response::EnumerateFields;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use uuid::Uuid;
 // use validator::Validate;
 
 /// the project model
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
 #[serde(rename_all = "camelCase")]
-pub struct ProjectsSchema {
+pub struct ProjectsModel {
     ///the project unique identifier
     pub id: Uuid,
     /// the project name
@@ -26,8 +24,26 @@ pub struct ProjectsSchema {
     pub live_url: String,
 }
 
+///for working with input and output
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ProjectInformation {
+    /// the project name
+    pub name: String,
+    ///the project description
+    pub description: String,
+    ///the technologies used, stored as array of string
+    pub technologies_used: Vec<String>,
+    ///the date the project was added to the database
+    pub date_added: String, //TODO: change to dateTime
+    ///the project repository url
+    pub repo_url: String,
+    ///the url of the deployed application if any
+    pub live_url: String,
+}
+
 ///implement enumerate fields for the project schema
-impl EnumerateFields for ProjectsSchema {
+impl EnumerateFields for ProjectInformation {
     /* return a key value pair of the the entries
      * to avoid borrow checker error and possible error from dereferencing,
      * clone the values of the struct
