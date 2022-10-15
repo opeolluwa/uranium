a3bb8c2749c4b7a9b720c7fac55053fd223ef6b7
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-10-15 13:44:31 +0100

    UPDATE

---
+++ views/assets/NotificationView.ad16654c.js
@@ -0,0 +1,1 @@
+import{A as r}from"./AppNetworkError.cc097bf9.js";import{d as e,_ as t,r as n,o as p,c}from"./index.16203f2c.js";const s=e({name:"NotificationView",components:{AppNetworkError:r}});function a(i,_,f,m,d,k){const o=n("AppNetworkError");return p(),c(o)}const l=t(s,[["render",a]]);export{l as default};
