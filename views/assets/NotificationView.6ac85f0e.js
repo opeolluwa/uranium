8c09896e24a93cb0543ddf147f002a9969ef4b5e
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-11-05 22:37:11 +0100

    UPDATE

--- views/assets/NotificationView.6ac85f0e.js
+++
@@ -1,1 +0,0 @@
-import{d as n,_ as o}from"./index.679e4c1f.js";const t=n({name:"NotificationView",components:{}});function e(i,c,s,a,r,f){return null}const _=o(t,[["render",e]]);export{_ as default};
---
+++ views/assets/web.d3c5bdb5.js
@@ -0,0 +1,1 @@
+import{a4 as n}from"./index.682f51d2.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
