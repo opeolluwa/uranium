cc408d65eaafd708e0daec26ee80e37762d7aa3c
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-12-19 18:40:03 +0100

    code clean up
    fixed lintting errors, updated email templates

--- src/shared/otp_handler.rs
+++ src/shared/otp_handler.rs
@@ -15,7 +15,7 @@ static OTP: Lazy<TOTP> = Lazy::new(|| -> TOTP {
 /// set otp validity period to 5 minutes
 const OTP_VALIDITY: u64 = 60 * 5; // 5 minutes
                                   //get the current time stamp
-const CURRENT_TIMESTAMP: Lazy<u64> = Lazy::new(|| {
+static CURRENT_TIMESTAMP: Lazy<u64> = Lazy::new(|| {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
