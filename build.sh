b8c3743ba735dcdf5d2094550da8f096088e9fa5
ADEOYE Adefemi <adefemiadeoye@yahoo.com>
2023-01-22 16:56:59 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-# build the project static files
-cd ../nitride-ui &&  npm run build
-cd ../nitride
-# copy the static files to the server
-rm -rf views/*
-cp -r ../nitride-ui/dist/* ./views
