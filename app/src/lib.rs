b02e993a57e1ecdd3911141d56b0caeb1caf424c
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-08-05 03:58:09 +0100

    chore: added dependencies

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-hello-world
+//! ```
+
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+// #[tokio::main]
+pub async fn run() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
