e10857c7038649fbaf22fa979bd3ecdc23bf704e
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-09-01 12:57:11 +0100

    chore: added comment

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use app::Uranium;
+
+/// Uranium is an authentication server for microservices
+/// it is built primarily on gRPC transport layer
+/// it also implement other protocol for communication of different layers
+///
+/// SMTP - for email transporter
+/// AMQP - for message queue  system
+/// HTTPS - the HTTP is used for
+
 #[tokio::main]
 async fn main() {
-    app::run().await;
+    // run the server
+    Uranium::run().await;
 }
